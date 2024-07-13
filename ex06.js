dee05264711f169015d3333a7d5839967fb03c08
Camponez <thiagotradercamponez@gmail.com>
2024-07-12 21:40:58 -0300

    Finalizado o ex06

---
+++ ex06.js
@@ -0,0 +1,36 @@
+let nota = 5;
+
+switch (nota) {
+    case 10:
+        console.log('Excelente');
+        break;
+    case 9:
+        console.log('Muito bom');
+        break;
+    case 8:
+        console.log('Bom');
+        break;
+    case 7:
+        console.log('Regular');
+        break;
+    case 6:
+        console.log('Mediano');
+        break;
+    case 5:
+        console.log('Pode melhorar');
+        break;
+    case 4:
+        console.log('Deve melhorar');
+        break;
+    case 3:
+        console.log('Está ruim');
+        break;
+    case 2:
+        console.log('Muito ruim');
+        break;
+    case 1:
+        console.log('Está horrível');
+        break;
+    default:
+        console.log('Nota inválida');
+}
