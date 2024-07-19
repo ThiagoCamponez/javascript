d2e3645823655cffc11cdc2c8aa4d7e4c21ecd85
Camponez <thiagotradercamponez@gmail.com>
2024-07-19 20:05:56 -0300

    Continuando exercício

--- ex015.js
+++ ex015.js
@@ -1,14 +1,18 @@
 function funcaoDesconto(nomeCliente, valorTotalCompra, primeiraCompra, compraAvista) {
     let desconto = 0;
-    if (primeiraCompra === true && compraAvista === true){
-        desconto = valorTotalCompra * 0.30;
-        
+    let percentualDesconto = 0;
+    if (primeiraCompra === true && compraAvista === true && valorTotalCompra > 1000) {
+        percentualDesconto = 0.30;
+        desconto = valorTotalCompra * percentualDesconto;        
+    } else if(primeiraCompra === true && compraAvista === true && valorTotalCompra < 1000 && valorTotalCompra > 500){
+        percentualDesconto = 0.20;
+        desconto = valorTotalCompra * percentualDesconto;
     }
 
     return console.log(`Obrigado pela compra ${nomeCliente}!
     Valor da compra: ${valorTotalCompra},00
-    Desconto de 30%: ${desconto}
+    Desconto de ${percentualDesconto * 100}%: ${desconto}
     Valor a pagar: R$${valorTotalCompra - desconto}`)
 }   
 
-funcaoDesconto('Thiago', 5000, true, true)
+funcaoDesconto('Thiago', 980, true, true)
