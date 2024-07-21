cceee54b170f31b3cf657355d92a0b55428e23bf
Camponez <thiagotradercamponez@gmail.com>
2024-07-21 20:42:12 -0300

    Outra forma de resolução

--- ex09.js
+++ ex09.js
@@ -1,14 +1,11 @@
 /*Escreva um programa onde, chama uma função enviando um número, a função imprime do 1 ate o número informado*/
 
-function sequencial(num){
-    let n1 = 1
-
-    for (let i = 0; i < num; i++){
-        console.log(n1)
-        n1 += 1
+const sequencial = (num) => {
+    for (i = 1; i < num; i++){
+        console.log('Contando...' + i)
     }
 
-    return n1
+    return i
 }
 
-sequencial(100)
+sequencial(10)
