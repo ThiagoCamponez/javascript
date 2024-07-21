987e59a6054fd806a7961213131e6e63696ea3ca
Camponez <thiagotradercamponez@gmail.com>
2024-07-21 20:35:07 -0300

    Finalizado o exercício dos descontos

--- ex015.js
+++ ex015.js
@@ -1,13 +1,46 @@
 function funcaoDesconto(nomeCliente, valorTotalCompra, primeiraCompra, compraAvista) {
     let desconto = 0;
     let percentualDesconto = 0;
-    if (primeiraCompra === true && compraAvista === true && valorTotalCompra > 1000) {
-        percentualDesconto = 0.30;
-        desconto = valorTotalCompra * percentualDesconto;        
-    } else if(primeiraCompra === true && compraAvista === true && valorTotalCompra < 1000 && valorTotalCompra > 500){
-        percentualDesconto = 0.20;
-        desconto = valorTotalCompra * percentualDesconto;
+    if (primeiraCompra){
+        if (compraAvista){
+            if (valorTotalCompra > 1000){
+                percentualDesconto = 0.30;
+            } else if (valorTotalCompra > 500){
+                percentualDesconto = 0.25;
+            } else {
+                percentualDesconto = 0.20;
+            }
+        } else { 
+            if (valorTotalCompra > 1000){
+                percentualDesconto = 0.20;
+            } else if (valorTotalCompra > 500){
+                percentualDesconto = 0.15;
+            } else {
+                percentualDesconto = 0.10;
+            }
+        }
+    } else {
+        if (compraAvista){
+            if (valorTotalCompra > 1000){
+                percentualDesconto = 0.20;
+            } else if (valorTotalCompra > 500){
+                percentualDesconto = 0.15;
+            } else {
+                percentualDesconto = 0.10;
+            }
+        } else {
+            if (valorTotalCompra > 1000){
+                percentualDesconto = 0.10;
+            } else if (valorTotalCompra > 500){
+                percentualDesconto = 0.05;
+            } else {
+                percentualDesconto = 0.00;
+                console.log('Compra sem desconto.');
+            }
+        }
     }
+
+    desconto = valorTotalCompra * percentualDesconto;
 
     return console.log(`Obrigado pela compra ${nomeCliente}!
     Valor da compra: ${valorTotalCompra},00
@@ -15,4 +48,4 @@ function funcaoDesconto(nomeCliente, valorTotalCompra, primeiraCompra, compraAvi
     Valor a pagar: R$${valorTotalCompra - desconto}`)
 }   
 
-funcaoDesconto('Thiago', 980, true, true)
+funcaoDesconto('Thiago', 480, false, false)
