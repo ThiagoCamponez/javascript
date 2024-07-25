fe18341281362a400f371c08726b058e2667add3
ThiagoCamponez <124745669+ThiagoCamponez@users.noreply.github.com>
2024-07-25 09:52:08 -0300

    Finalizando os Methods de arrays

--- arrayMethods.js
+++ arrayMethods.js
@@ -4,13 +4,17 @@ const students = ['Thiago', 'João', 'Maria', 'Pedro', 'Ana'];
 students.push('Maurício'); // adiciona elemento no final do array
 students.unshift('Elisa'); // adiciona elemento no comeco do array
 students.pop(); // remove elemento do final do array
+students.shift(); // remove elemento do comeco do array
 students.length(); // tamanho do array
 students.sort(); // ordena o array
 students.delete(0); // remove elemento pelo indice
 students.splice(0, 2); // remove dois elementos apartir do indice 0
 students.splice(1, 0, 'Laís'); // adiciona elemento no indice 1
 students.splice(1, 1, 'Laís'); // substitui elemento no indice 1
+const newStudents = students.slice(1, 3); // cria um novo array com elementos entre os indices 1 e 3 (tem que criar um novo array para fazer isso)
+
 
 */
-students.splice(1, 0, 'Laís');
+students.shift()
+
 console.log(students)
